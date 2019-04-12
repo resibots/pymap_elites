99a41562e9afcf207836ccc8196e1421399df215
DALIN Eloise <eloise.dalin@inria.fr>
2019-04-12 16:12:10 +0200

    added license

--- map_elites.py
+++ map_elites.py
@@ -1,3 +1,41 @@
+#! /usr/bin/env python
+#| This file is a part of the pymap_elites framework.
+#| Copyright 2019, INRIA
+#| Main contributor(s):
+#| Jean-Baptiste Mouret, jean-baptiste.mouret@inria.fr
+#| Eloïse Dalin , eloise.dalin@inria.fr
+#| Pierre Desreumaux , pierre.desreumaux@inria.fr
+#|
+#|
+#| **Main paper**: Mouret JB, Clune J. Illuminating search spaces by
+#| mapping elites. arXiv preprint arXiv:1504.04909. 2015 Apr 20.
+#|
+#| This software is governed by the CeCILL license under French law
+#| and abiding by the rules of distribution of free software.  You
+#| can use, modify and/ or redistribute the software under the terms
+#| of the CeCILL license as circulated by CEA, CNRS and INRIA at the
+#| following URL "http://www.cecill.info".
+#|
+#| As a counterpart to the access to the source code and rights to
+#| copy, modify and redistribute granted by the license, users are
+#| provided only with a limited warranty and the software's author,
+#| the holder of the economic rights, and the successive licensors
+#| have only limited liability.
+#|
+#| In this respect, the user's attention is drawn to the risks
+#| associated with loading, using, modifying and/or developing or
+#| reproducing the software by the user in light of its specific
+#| status of free software, that may mean that it is complicated to
+#| manipulate, and that also therefore means that it is reserved for
+#| developers and experienced professionals having in-depth computer
+#| knowledge. Users are therefore encouraged to load and test the
+#| software's suitability as regards their requirements in conditions
+#| enabling the security of their systems and/or data to be ensured
+#| and, more generally, to use and operate it in the same conditions
+#| as regards security.
+#|
+#| The fact that you are presently reading this means that you have
+#| had knowledge of the CeCILL license and that you accept its terms.
 from sklearn.neighbors import KDTree
 from sklearn.cluster import KMeans
 import math
