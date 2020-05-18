b05c5c12d55dc4447ff71e06e195282ea6ba0588
vltempez-local <vltempez-local@k2so.loria.fr>
2020-05-18 14:28:27 +0200

    add min and max to point info

--- examples/read_sensitivity.py
+++ examples/read_sensitivity.py
@@ -6,5 +6,8 @@ def read_data(filename):
     data = [] 
     for i in range(1, len(lines), 2):
         line = lines[i].split(';')
-        data.append({'noise_level' : float(line[0]), 'fitness_mean': float(line[1]), 'fitness_var': float(line[2][:-1])})
+        line = lines[i+1][1:-2].split(',')
+        fitness_values = [float(s) for s in fitness_values]
+        filtered_fitness_values = [v for v in fitness_values if v> -float('inf')]
+        data.append({'noise_level' : float(line[0]), 'fitness_mean': float(line[1]), 'fitness_var': float(line[2][:-1]), 'min': min(filtered_fitness_values), "max": max(filtered_fitness_values)})
     return data
