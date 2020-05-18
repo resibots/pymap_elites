c89d8ecbb3fc0179bfcb8c18f21863c45cd92fcd
vltempez-local <vltempez-local@k2so.loria.fr>
2020-05-18 14:31:00 +0200

    correct line reading error

--- examples/read_sensitivity.py
+++ examples/read_sensitivity.py
@@ -5,9 +5,9 @@ def read_data(filename):
     title_line  = lines[0]
     data = [] 
     for i in range(1, len(lines), 2):
-        line = lines[i].split(';')
-        line = lines[i+1][1:-2].split(',')
-        fitness_values = [float(s) for s in fitness_values]
+        line1 = lines[i].split(';')
+        line2 = lines[i+1][1:-2].split(',')
+        fitness_values = [float(s) for s in line2]
         filtered_fitness_values = [v for v in fitness_values if v> -float('inf')]
-        data.append({'noise_level' : float(line[0]), 'fitness_mean': float(line[1]), 'fitness_var': float(line[2][:-1]), 'min': min(filtered_fitness_values), "max": max(filtered_fitness_values)})
+        data.append({'noise_level' : float(line1[0]), 'fitness_mean': float(line1[1]), 'fitness_var': float(line1[2][:-1]), 'min': min(filtered_fitness_values), "max": max(filtered_fitness_values)})
     return data
